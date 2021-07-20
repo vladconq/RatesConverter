978328c1cdaaaf6ee66afef1db5704d35c07db69
Vladislav Bogucharov <vladconq@gmail.com>
2021-07-21 00:29:34 +0300

    Finish version 0.1

--- RatesConverter/ViewController.swift
+++ RatesConverter/ViewController.swift
@@ -8,17 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
     var currency: [String] = []
     var values: [Double] = []
-    
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        currency.count
-    }
     
     @IBOutlet weak var labelResult: UILabel!
     @IBOutlet weak var textFieldInput: UITextField!
@@ -27,6 +18,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     override func viewDidLoad() {
         super.viewDidLoad()
         pickerView.delegate = self
+        pickerView.dataSource = self
         getRates()
     }
     
@@ -42,10 +34,35 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
                 let data = try JSONDecoder().decode(Rates.self, from: data!)
                 self.currency.append(contentsOf: data.rates.keys)
                 self.values.append(contentsOf: data.rates.values)
+                
+                DispatchQueue.main.async {
+                    self.pickerView.reloadAllComponents()
+                }
             } catch {
                 return
             }
         }.resume()
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        currency.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currency[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        guard textFieldInput.text != nil else {return}
+//        print(Double(textFieldInput.text!)!)
+        labelResult.text = "\(values[row] * Double(textFieldInput.text!)!)"
+////        labelResult.text = currency[row]
+//        labelResult.text = "\(values[row])"
+//        print(currency[row])
     }
 
 }
