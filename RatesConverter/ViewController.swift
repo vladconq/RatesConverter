782f4f4835b12906c13853fc2b7ae193da103cbc
Vladislav Bogucharov <vladconq@gmail.com>
2021-07-20 23:25:32 +0300

    Initial Commit

---
+++ RatesConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RatesConverter
+//
+//  Created by Vladislav on 20.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
