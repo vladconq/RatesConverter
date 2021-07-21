4152ae6aa9e7a01e6ecb219d14e8fe07459a0cba
Vladislav Bogucharov <vladconq@gmail.com>
2021-07-21 16:37:45 +0300

    Improve behavior of UITextField

--- RatesConverter/ViewController.swift
+++ RatesConverter/ViewController.swift
@@ -7,9 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
     var currency: [String] = []
     var values: [Double] = []
+    var currentRate: Double = 0.0
     
     @IBOutlet weak var labelResult: UILabel!
     @IBOutlet weak var textFieldInput: UITextField!
@@ -19,6 +20,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         super.viewDidLoad()
         pickerView.delegate = self
         pickerView.dataSource = self
+        textFieldInput.delegate = self
         getRates()
     }
     
@@ -53,17 +55,26 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        currentRate = values[row]
         return currency[row]
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        guard textFieldInput.text != nil else {return}
-//        print(Double(textFieldInput.text!)!)
-        labelResult.text = "\(values[row] * Double(textFieldInput.text!)!)"
-////        labelResult.text = currency[row]
-//        labelResult.text = "\(values[row])"
-//        print(currency[row])
+        calculate()
     }
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        calculate()
+        self.view.endEditing(true)
+        return true
+    }
+    
+    func calculate() {
+        guard textFieldInput.text != "" else {return}
+        print(textFieldInput.text)
+        print(currentRate)
+        labelResult.text = "\(currentRate * Double(textFieldInput.text!)!)"
+    }
 }
+
 
